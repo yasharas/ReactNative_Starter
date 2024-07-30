f6f1cba3df194297a2b5682ea01b8737cedc1e9f
utkarshnadgouda <c-unadgouda@usmedequip.com>
2024-07-30 17:14:31 +0530

    changes

--- template/App.tsx
+++ template/App.tsx
@@ -87,7 +87,7 @@ function App(): React.JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Template test
           </Section>
           <LearnMoreLinks />
         </View>
